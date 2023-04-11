4f390906ca4cbeee5755ecb8f5ce659a909474c3
kidalonious <120538119+kidalonious@users.noreply.github.com>
2023-04-10 18:04:49 -0600

    Handful of changes

--- missingpersons/landing/urls.py
+++ missingpersons/landing/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, databasePageView
 
 urlpatterns = [
-    path("", indexPageView, name="index")
+    path("", indexPageView, name="index"),
+    path('database/', databasePageView, name='database')
 ]
--- missingpersons/landing/views.py
+++ missingpersons/landing/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def indexPageView (request) :
     return render(request, 'landing/index.html')
+
+def databasePageView (request) :
+    return render(request, 'landing/database.html')
