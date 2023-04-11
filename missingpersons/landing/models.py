fcb57628a6ff95851e9aeb0ad6d00974481bf096
kidalonious <120538119+kidalonious@users.noreply.github.com>
2023-04-11 17:15:13 -0600

    basic changes to styling/project details

--- missingpersons/landing/models.py
+++ missingpersons/landing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model) :
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    missing_age = models.IntegerField()
+    city = models.CharField(max_length=45)
+    state = models.CharField(max_length=45)
+    gender = models.CharField(max_length=1)
+    race = models.CharField(max_length=1)
--- missingpersons/missingpersons/settings.py
+++ missingpersons/missingpersons/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'missingpersons.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'missingpersons',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
     }
 }
 
