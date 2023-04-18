694af8e1dc5be80371613a334b28999452079a4f
afoote99 <120541736+afoote99@users.noreply.github.com>
2023-04-18 10:49:26 -0700

    Update urls.py
    linked url for each kids individual ID

--- missingpersons/landing/urls.py
+++ missingpersons/landing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import indexPageView, databasePageView, resourcePageView
+from .views import indexPageView, databasePageView, resourcePageView, individualPageView
 
 urlpatterns = [
     path('', indexPageView, name='index'),
     path('database/', databasePageView, name='database'),
-    path('resources/', resourcePageView, name='resources')
+    path('resources/', resourcePageView, name='resources'),
+    path('<int:id>/', individualPageView, name = 'individual'),
 ]
