dcdb183f8a9d33b201667285fd135e4db6bdd84e
afoote99 <120541736+afoote99@users.noreply.github.com>
2023-04-18 11:03:18 -0700

    Update models.py
    Tabbed in the return for the description

--- missingpersons/landing/models.py
+++ missingpersons/landing/models.py
@@ -13,4 +13,4 @@ class Person(models.Model) :
     race = models.CharField(max_length=1)
     
     def __str__(self) :
-    return(self.first_name + " " + self.last_name)
+        return(self.first_name + " " + self.last_name)
