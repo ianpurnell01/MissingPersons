3d11cf5ca67659a0d9accea7a22134862aea0812
afoote99 <120541736+afoote99@users.noreply.github.com>
2023-04-18 10:46:42 -0700

    Update views.py
    imported model
    Linked database html with the database from admin panel
    Created a function for the individual page view, linked it with each kids unique ID

--- missingpersons/landing/views.py
+++ missingpersons/landing/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-
+from .models import Person
 
 # Create your views here.
 def indexPageView (request) :
     return render(request, 'landing/index.html')
 
 def databasePageView (request) :
-    return render(request, 'landing/database.html')
+    people = Person.objects.all()
+
+    context = {
+        'data' : people
+    }
+
+    return render(request, 'landing/database.html', context)
 
 def resourcePageView (request) :
     return render(request, 'landing/resources.html')
+
+def individualPageView (request, id) :
+    missingIndividual = Person.objects.get(id=id)
+
+    context = {
+        'data' : missingIndividual
+    }
+
+    return render (request, 'landing/individual.html', context)
