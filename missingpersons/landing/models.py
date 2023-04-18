e37dd7cf50fb0e9817129bc0716ecfa8d6be5ddb
kidalonious <120538119+kidalonious@users.noreply.github.com>
2023-04-18 16:10:15 -0600

    mostly changes to the individuals.html file

--- missingpersons/landing/models.py
+++ missingpersons/landing/models.py
@@ -9,8 +9,10 @@ class Person(models.Model) :
     missing_age = models.IntegerField()
     city = models.CharField(max_length=45)
     state = models.CharField(max_length=45)
-    gender = models.CharField(max_length=1)
-    race = models.CharField(max_length=1)
+    gender = models.CharField(max_length=30)
+    race = models.CharField(max_length=30)
     
     def __str__(self) :
         return(self.first_name + " " + self.last_name)
+    
+    
